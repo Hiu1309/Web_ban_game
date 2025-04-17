6b3c6b6111a3606496922e79b319edcf11d57a52
Thinh <phanthanhthinh117@gmail.com>
2025-04-17 15:57:09 +0700

    admin

---
+++ admin/admin/gui/header.php
@@ -0,0 +1,13 @@
+<?php
+    echo'<div class="header">
+    Xin chào, Admin
+  </div>
+  
+  <div class="container">
+    <div class="sidebar">
+      <a href="admin.php?page=add">Thêm</a>
+      <a href="admin.php?page=edit">Sửa</a>
+      <a href="admin.php?page=delete">Xóa</a>
+      <a href="admin.php?page=permission">Quyền</a>
+    </div>';
+?>
